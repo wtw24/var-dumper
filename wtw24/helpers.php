3a8eb167a5f7af600d93c47bd1406302aede6c54
wtw24 <wladimir.wendland@mediafinanz.de>
2019-02-25 16:20:49 +0100

    Add check if function_exists

--- wtw24/helpers.php
+++ wtw24/helpers.php
@@ -1,20 +1,24 @@
 <?php
 
-function d()
-{
-    array_map(function ($x) {
-        $damper = new \WTW24\Support\Debug\Dumper;
-        $damper->dump($x);
-    }, func_get_args());
+if (!function_exists('d')) {
+    function d()
+    {
+        array_map(function ($x) {
+            $damper = new \WTW24\Support\Debug\Dumper;
+            $damper->dump($x);
+        }, func_get_args());
+    }
 }
 
+if (!function_exists('dd')) {
+    function dd()
+    {
+        array_map(function ($x) {
+            $damper = new \WTW24\Support\Debug\Dumper;
+            $damper->dump($x);
+        }, func_get_args());
 
-function dd()
-{
-    array_map(function ($x) {
-        $damper = new \WTW24\Support\Debug\Dumper;
-        $damper->dump($x);
-    }, func_get_args());
-
-    die(1);
+        die(1);
+    }
 }
+
